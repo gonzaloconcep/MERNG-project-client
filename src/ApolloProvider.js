3e3b9d1e4404da6549aea7383d704b264643b64f
gonzaloconcep <lasconchasgonzalo@gmail.com>
2021-01-18 20:10:43 +0100

    Edited proxy and server base URL

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -9,7 +9,7 @@ import {
 import { setContext } from "apollo-link-context";
 
 const httpLink = createHttpLink({
-  uri: "http://localhost:5000",
+  uri: "https://tranquil-refuge-21379.herokuapp.com/",
 });
 
 const authLink = setContext(() => {
